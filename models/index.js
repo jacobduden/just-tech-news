2abfb40d846e96edf69f7ca0c6cd05ff2735cd9b
jacobduden <75395688+jacobduden@users.noreply.github.com>
2021-05-20 14:26:56 -0500

    made routes for post model

--- models/index.js
+++ models/index.js
@@ -1,4 +1,22 @@
 const User = require('./User');
 const Post = require("./Post");
+const router = require('express').Router();
 
+const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
+
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+
+  Post.belongsTo(User, {
+    foreignKey: 'user_id',
+  });
+  
+
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+
+module.exports = router;
 module.exports = { User, Post };
